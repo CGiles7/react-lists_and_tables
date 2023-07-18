94f8026ca423dad55d916bd54f574ea5c00b5341
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-18 13:09:39 -0400

    add table code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+import React from "react";
+
+const todos = [
+  { completed: false, description: "Finish the Lists & Tables checkpoint" },
+  { completed: false, description: "Clean my desk" },
+  { completed: false, description: "Make lunch" },
+];
 
 function App() {
-  return <h1>Hello World!</h1>;
-}
+  const rows = todos.map(({ completed, description }, index) => (
+    <tr key={index}>
+      <td>{description}</td>
+      <td>{completed ? "Yes" : "No"}</td>
+    </tr>
+  ));
 
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Completed?</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+//tr=table row
+//th=table header item(in box)
+//td=table data item(in box)
 export default App;
